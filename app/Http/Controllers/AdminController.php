0de8ecdf575d25bfce9535e647aa8c091ce2a338
ggapple <thuanmach123@gmail.com>
2025-08-13 16:00:20 +0700

    create project and add adminController

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function getIndex()
+    {
+        if (Auth::id()) {
+            $usertype = Auth()->user()->usertype;
+            if ($usertype == 'user') {
+                return view('dashboard');
+            } elseif ($usertype == 'admin') {
+                return view('admin.index');
+            } else {
+                return redirect()->back();
+            }
+        }
+    }
+}
