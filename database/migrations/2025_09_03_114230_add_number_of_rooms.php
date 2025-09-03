94ee7efd7307d6368b4d56997c60069208bb7c00
ggapple <thuanmach123@gmail.com>
2025-09-03 18:49:24 +0700

    add number of rooms in DB

---
+++ database/migrations/2025_09_03_114230_add_number_of_rooms.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->string('num_of_room')->default('10')->after('type');
+            $table->string('num_of_rented')->default('0')->after('num_of_room');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->dropColumn('num_of_rooms');
+            $table->dropColumn('num_of_rented');
+        });
+    }
+};
